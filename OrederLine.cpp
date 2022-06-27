bf6986a7c37ddaef47ff7eb16cf0793c385ba059
daun <2579804037@qq.com>
2022-06-27 21:24:31 +0800

    "整理vscode环境"

--- OrederLine.cpp
+++ OrederLine.cpp
@@ -1,2 +1,41 @@
 #include "duan.h"
+using namespace orderline;
 
+bool gennerlist(SeqList &L, int n);
+int Rand_range(int a,int b);
+
+//P17 T1 删除最小的元素
+int mian(){
+    SeqList L,&list=L;
+    ElemType e,&elem=e;
+    int n=10;
+    InitList(list);
+    gennerlist(list,n);
+    printf("/n顺序表数列：");
+    PrintList(L);
+    if(Empty(L)){
+        printf("\nEmpty list!");
+        return 0;
+    }
+    else{
+        ListDelete(list,LocateMin(L),elem);
+        printf("\n删除了最小元素%d",elem);
+        return 1;
+    }
+}
+
+//生成随机n个元素的随机队列
+bool gennerlist(SeqList &L, int n){
+    ElemType e;
+    for(int i=0;i<n;i++){
+        e=Rand_range(0,20);
+        if(!ListInsert(L,L.length+1,e)){
+            return false;
+        }
+    }
+    return true;
+}
+//随机a到b的数
+int Rand_range(int a,int b){
+    return a+rand()%(b-a);
+}
--- duan.h
+++ duan.h
@@ -1,101 +1,122 @@
 #include<iostream>
+#include<cstdlib>
 using namespace std;
-
 #define InitSize 100
-
 #define ElemType int
-//线性表结构体
-typedef struct{
-    ElemType *data;
-    int MaxSize,length;
-} SeqList;
 
-//线性表初始化
-void InitList(SeqList &L){
-    L.data = new ElemType[InitSize];
-    L.length =0;
-    L.MaxSize = InitSize;//最大长度为初始设定
-}
+namespace orderline{ 
+    //线性表结构体
+    typedef struct{
+        ElemType *data;
+        int MaxSize,length;
+    } SeqList;
 
-//线性表长度
-int Length(SeqList L){
-    return L.length;
-}
-
-//定位元素
-int LocateElement(SeqList L, ElemType e){
-    for(int i=0; i<L.length; i++){
-        if(L.data[i]==e){
-            return i+1;
-        }
+    //线性表初始化
+    void InitList(SeqList &L){
+        L.data = new ElemType[InitSize];
+        L.length =0;
+        L.MaxSize = InitSize;//最大长度为初始设定
     }
-    return -1;
-}
 
-//得到第i个元素
-ElemType GetElem(SeqList L,int i){
-    if(i>=0 && i<L.length){
-        return L.data[i];
+    //线性表长度
+    int Length(SeqList L){
+        return L.length;
     }
-    else{
-        return 0;
-    }
-}
 
-//插入在第i个元素的位置
-bool ListInsert(SeqList &L, int i, ElemType e){
-    if(i>=1 && i<=L.length && L.length+1<=L.MaxSize){
-        for(int j=L.length; j>=i; j--){
-            L.data[j] = L.data[j-1];
-        }
-        L.data[i-1]=e;
-        L.length++;
-        return true;
-    }
-    else
-        return false;
-}
-
-//删除第i个元素
-bool ListDelete(SeqList &L, int i, ElemType &e){
-    if(i>=1 && i<=L.length){
-        e=L.data[i-1];
-        for(int j=i; j<L.length; j++){
-            L.data[j-1]=L.data[j];
-        }
-        L.length--;
-        return true;
-    }
-    else{
-        return false;
-    }
-}
-
-//输出线性表元素
-bool PrintList(SeqList L){
-    if(L.length>=1){
+    //定位元素
+    int LocateElement(SeqList L, ElemType e){
         for(int i=0; i<L.length; i++){
-            if (i%10==0 && i!=0){
-                cout <<"\n";
+            if(L.data[i]==e){
+                return i+1;
             }
-            cout << L.data[i]<<"\t";
         }
-        return true;
+        return -1;
     }
-    else 
-    return false;
-}
 
-//判空
-bool Empty(SeqList L){
-    if(L.length=0){
-        return true;
+    //得到第i个元素
+    ElemType GetElem(SeqList L,int i){
+        if(i>=0 && i<L.length){
+            return L.data[i];
+        }
+        else{
+            return 0;
+        }
     }
-    else 
+
+    //插入在第i个元素的位置
+    bool ListInsert(SeqList &L, int i, ElemType e){
+        if(i>=1 && i<=L.length && L.length+1<=L.MaxSize){
+            for(int j=L.length; j>=i; j--){
+                L.data[j] = L.data[j-1];
+            }
+            L.data[i-1]=e;
+            L.length++;
+            return true;
+        }
+        else
+            return false;
+    }
+
+    //删除第i个元素
+    bool ListDelete(SeqList &L, int i, ElemType &e){
+        if(i>=1 && i<=L.length){
+            e=L.data[i-1];
+            for(int j=i; j<L.length; j++){
+                L.data[j-1]=L.data[j];
+            }
+            L.length--;
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    //输出线性表元素
+    bool PrintList(SeqList L){
+        if(L.length>=1){
+            for(int i=0; i<L.length; i++){
+                if (i%10==0 && i!=0){
+                    cout <<"\n";
+                }
+                cout << L.data[i]<<"\t";
+            }
+            return true;
+        }
+        else 
         return false;
-}
+    }
 
-bool DestroyList(SeqList &L){
-    delete L.data;
-    free(&L);
+    //判空
+    bool Empty(SeqList L){
+        if(L.length==0){
+            return true;
+        }
+        else 
+            return false;
+    }
+
+    //销毁线性表
+    bool DestroyList(SeqList &L){
+        delete L.data;
+        L.length=0;
+        free(&L);
+        if(&L){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    //定位最小元素的位置
+    int LocateMin(SeqList L){
+        int min_index=0;
+        for(int i=1; i<L.length; i++){
+            if(L.data[min_index]>L.data[i]){
+                min_index=i;
+            }
+        }
+        return min_index+1;
+    }
 }
